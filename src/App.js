22d41cf16c1e39befbfe0880cc20f3db4adb0c8b
jiwon <blackswan_s@naver.com>
2023-01-15 09:41:00 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/*eslint-disable */ //Lint끄는 기능
+
+
 import logo from './logo.svg';
 import './App.css';     //css 파일경로
 import { useState } from 'react';
@@ -7,6 +10,11 @@ function App() {
 
   let post = '울산 우동 맛집';
   let [글제목, b] = useState(['여자 코트 추천', '강남 우동맛집', '리액트독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  // function 함수(){
+  //   console.log(1);
+  // }
 
 
   return (
@@ -15,9 +23,12 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick={() => { 따봉변경(따봉+1)}}>💖</span> {따봉} </h4>
         <p>1월 15일 발행</p>
       </div>
+
+
+
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>1월 15일 발행</p>
